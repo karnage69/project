121448881512f59d6fd6f82eab445a525e83deea
Ayush <mathurayush541@gmail.com>
2025-01-16 22:12:10 +0530

    Updated chapter4.py with new code

--- chapter4.py
+++ chapter4.py
@@ -1,6 +1,44 @@
-def add(a, b):
-    return a + b
+#WAP tp store seven fruits stored by user 
+"""l= []
+a = input("enter severn fruites names: ")
+l.append(a)
+b = input("enter severn fruites names: ")
+l.append(b)
+c = input("enter severn fruites names: ")
+l.append(c)
+d = input("enter severn fruites names: ")
+l.append(d)
+e = input("enter severn fruites names: ")
+l.append(e)
+f = input("enter severn fruites names: ")
+l.append(f)
+g = input("enter severn fruites names: ")
+l.append(g)
+h = input("enter severn fruites names: ")
+l.append(h)
+print(f"the list is {l}")"""
 
-if __name__ == "__main__":
-    print("5 + 3 =", add(5, 3))
-    
+#WAP to accept marks of 6 students and display them in sorted manner 
+"""l = []
+marks1=float(input("enter the marks: "))
+l.append(marks1)
+marks2=float(input("enter the marks:"))
+l.append(marks2)
+marks3=float(input("enter the marks: "))
+l.append(marks3)
+marks4=float(input("enter the marks: "))
+l.append(marks4)
+marks5=float(input("enter the marks: "))
+l.append(marks5)
+marks6=float(input("enter the marks: "))
+l.append(marks6)
+l.sort()
+print (f"all sorted marks are{l}")"""
+
+#WAP to sum a list with 4 numbers
+"""l = [2,3,4,5]
+print(sum(l))"""
+#WAP to count the number of zeroes in list 
+l = [7,0,8,0,0,9]
+n= l.count(0)
+print(n)
